fe6d023d17600636453ca8d044ccb9aa44931a1b
Adrian Gette <adrian.gette@icloud.com>
2022-09-11 21:45:44 -0300

    Compound component pattern
    Compound component pattern

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ProductCard, { ProductButtons, ProductImage, ProductTitle } from '../components/ProductCard'
+
+const product = {
+    id: '1',
+    title: 'Producto 1',
+    img: './coffee-mug.png'
+}
+
+const ShoppingPage = () => {
+    return (
+        <div>
+            <h1>Shopping Store</h1>
+            <hr />
+
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                <ProductCard product={product}>
+                    <ProductCard.Image />
+                    <ProductCard.Title title={ "" } />
+                    <ProductCard.Buttons />
+                </ProductCard>
+
+                <ProductCard product={product}>
+                    <ProductImage />
+                    <ProductTitle />
+                    <ProductButtons />
+                </ProductCard>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ShoppingPage
